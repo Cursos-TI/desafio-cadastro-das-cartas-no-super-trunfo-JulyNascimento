3c4acbb35d46ff6e4589de925eb493417b876528
JulyNascimento <Julyevangelistaa@gmail.com>
2025-08-14 13:07:29 +0000

    estado B conclusão

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,11 +1,33 @@
 #include    <stdio.h>
 
     int main(){
+        char Estado = 'B';
+        char CódigoDacarta[5] = "B01";
+        char NomeDaCidade[10] = "SALVADOR";
+        int População = 14850513;
+        float ÁreaEmkm² = 564.760;
+        float Pib = 138.5;
+        int NúmeroDePontosTurísticos = 70;
 
 
+        printf("Estado: %c\n",Estado);
+        printf("Código Da carta: %c\n",CódigoDacarta);
+        printf("Nome Da Cidade: %c\n",NomeDaCidade);
+        printf("População: %d\n",População);
+        printf("Área Em Km²: %.2f\n",ÁreaEmkm²);
+        printf("Pib: %.2f\n",Pib);
+        printf("Número De Pontos Turísticos: %d\n",NúmeroDePontosTurísticos);
 
 
+        scanf("%c", &Estado);
+        scanf("%c", &CódigoDacarta);
+        scanf("%c", &NomeDaCidade);
+        scanf("%d", &População);
+        scanf("%.2f", &ÁreaEmkm²);
+        scanf("%.2f", &Pib);
+        scanf("%d", &NúmeroDePontosTurísticos);
 
+        return 0;
 
         
     }
